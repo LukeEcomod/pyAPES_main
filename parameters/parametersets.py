36b63d438b25a10bd215fec9f5477acebb4f608a
khaahti <kersti.leppa@luke.fi>
2021-09-29 16:30:02 +0300

    parameters accoording to Leppa et al 2020

--- parameters/parametersets.py
+++ parameters/parametersets.py
@@ -5,6 +5,14 @@ Created on Fri Oct 19 12:39:09 2018
 
 @author: ajkieloaho
 """
+from pyAPES_utilities.parameter_utilities import single_lad_profiles
+from parameters.Lettosuo import grid
+
+hs = 0.5  # height of understory shrubs [m]
+control = single_lad_profiles(grid, 'pyAPES_utilities/runkolukusarjat/letto2014.txt',
+                              hs, plot=False, biomass_function='aleksis_combination')
+partial = single_lad_profiles(grid, 'pyAPES_utilities/runkolukusarjat/letto2016_partial.txt',
+                              hs, plot=False, biomass_function='aleksis_combination')
 
 # Modifications to some parameters
 
@@ -15,14 +23,26 @@ def get_parameters(scenario):
             'count': 1,
             'scenario': 'test',
             'general':{
-                'start_time' : "2005-06-01",
-                'end_time' : "2005-06-10",
-                'forc_filename' : "Hyytiala/FIHy_forcing_2005-2010.dat"
+                'start_time' : "2010-06-01",
+                'end_time' : "2010-06-10",
             },
             'canopy': {
                 'planttypes': {
+                    'pine': {
+                        'LAImax': (control['lai']['pine'], partial['lai']['pine']),
+                        'lad': (control['lad']['pine'], partial['lad']['pine']),
+                    },
+                    'spruce': {
+                        'LAImax': (control['lai']['spruce'], partial['lai']['spruce']),
+                        'lad': (control['lad']['spruce'], partial['lad']['spruce']),
+                    },
                     'decid': {
-                        'LAImax': (1.2),
+                        'LAImax': (control['lai']['decid'], partial['lai']['decid']),
+                        'lad': (control['lad']['decid'], partial['lad']['decid']),
+                    },
+                    'shrubs': {
+                        'LAImax': (1.0, 0.8),
+                        'lad': (control['lad']['shrubs'], partial['lad']['shrubs']),
                     },
                 },
             },
--- run_example.py
+++ run_example.py
@@ -22,7 +22,7 @@ from pyAPES_utilities.plotting import plot_fluxes
 
 # Get parameters and forcing for SMEAR II -site
 
-from parameters.SmearII import gpara, cpara, spara
+from parameters.Lettosuo import gpara, cpara, spara
 
 #  wrap parameters in dictionary
 params = {
@@ -41,11 +41,22 @@ outputfile, Model = driver(parameters=params, create_ncf=True)
 results = read_results(outputfile)
 
 # import fluxdata
-flxdata = read_data("forcing/Hyytiala/FIHy_flx_2005-2010.dat", sep=';',
-                       start_time=results.date[0].values, end_time=results.date[-1].values)
+flxdata = read_data('forcing/Lettosuo/Lettosuo_EC_2010_2018.csv', sep=',',
+                     start_time=results.date[0].values, end_time=results.date[-1].values)
+flxdata.index = flxdata.index - pd.Timedelta(hours=0.5)  # period end
+for trmt in ['control_','partial_']:
+    flxdata[trmt+'NEE'] *= 1.0 / 44.01e-3
+    flxdata[trmt+'Reco'] *= 1.0 / 44.01e-3
+    flxdata[trmt+'GPP'] = flxdata[trmt+'Reco'] - flxdata[trmt+'NEE']
+
+results['ground_heat_flux'] = results['soil_heat_flux'].isel(soil=6).copy()
 
 plot_fluxes(results, flxdata, norain=True,
-            res_var=['canopy_Rnet','canopy_SH','canopy_LE',
+            res_var=['canopy_Rnet','canopy_SWnet','canopy_SH','canopy_LE','ground_heat_flux',
                       'canopy_NEE','canopy_GPP','canopy_Reco'],
-            Data_var=['Rnet','H','LE','NEE','GPP','Reco'],
+            Data_var=['control_NRAD','control_NSWRAD','control_SH','control_LE','control_GHF',
+                      'control_NEE','control_GPP','control_Reco'],
             fmonth=5, lmonth=9, sim_idx=0)
+
+plt.figure()
+results['soil_ground_water_level'].plot.line(x='date')
