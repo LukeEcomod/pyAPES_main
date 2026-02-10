40ba23b859dcb68b121a8824d5087113cc7cac56
Altesmi <optikkasalo@gmail.com>
2026-02-10 11:27:02 +0200

    Profiling started

---
+++ run_hyytiala_2018_lad profile.py
@@ -0,0 +1,18 @@
+from pyAPES.pyAPES_MLM import driver
+from pyAPES.parameters.parameter_tools import get_parameter_list
+from pyAPES.parameters.SmearII_parameters import gpara, cpara, spara
+import cProfile
+
+#  wrap parameters in dictionary
+params = {
+    'general': gpara,
+    'canopy': cpara,
+    'soil': spara
+}
+
+# parameters simulation(s)
+params = get_parameter_list(params, 'hyytiala_2018_lad')
+print(params[0]['general'])
+# run model
+
+cProfile.run(driver(parameters=params, create_ncf=True, result_file='FiHy2018_lad_profile.nc'), 'lad_run_profile')
