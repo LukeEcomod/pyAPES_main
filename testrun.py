5b0251c9fea5c9112648a6c5925fadf1255b7e3f
Kersti Leppä <kersti.leppa@luke.fi>
2026-02-27 11:24:51 +0200

    SW radiation solution optimized wiht solve_banded

--- testrun.py
+++ testrun.py
@@ -2,8 +2,8 @@ from pyAPES.pyAPES_MLM import driver
 from pyAPES.parameters.mlm_parameters import gpara, cpara, spara
 from pyAPES.utils.iotools import read_forcing
 
-gpara['start_time'] = "2007-04-01"
-gpara['end_time'] = "2007-04-30"
+gpara['start_time'] = "2007-06-01"
+gpara['end_time'] = "2007-06-10"
 
 forcing = read_forcing(
     forcing_file=gpara['forc_filename'],
@@ -20,4 +20,4 @@ params = {
 }
 
 # run model
-outputfile, Model = driver(parameters=params, create_ncf=True, result_file= 'testrun_test.nc')
+outputfile, Model = driver(parameters=params, create_ncf=True, result_file= 'testrun_test_june.nc')
