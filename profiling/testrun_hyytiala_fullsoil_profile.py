45a0b82d21ee48b54311ed5439b4fd389a198f58
Altesmi <optikkasalo@gmail.com>
2026-02-27 11:25:39 +0200

    small changes

--- profiling/testrun_hyytiala_fullsoil_profile.py
+++ profiling/testrun_hyytiala_fullsoil_profile.py
@@ -22,10 +22,10 @@ params = {
 params = get_parameter_list(params, 'hyytiala_2018_lad')
 print(params[0]['general'])
 
+# run model with profiling
+
+cProfile.run("driver(parameters=params, create_ncf=True, result_file= 'testrun_fullsoil_profile_moss_iteration_hackathon_day3.nc')","lad_run_profile_long_fullsoil_moss_iteration_hackathon_day3.prof")
+
 # run model
-
-cProfile.run("driver(parameters=params, create_ncf=True, result_file= 'testrun_fullsoil_profile_moss_iteration_hackathon_day1.nc')","lad_run_profile_long_fullsoil_moss_iteration_hackathon_day1.prof")
-
-# # run model
-# outputfile, Model = driver(parameters=params, create_ncf=True, result_file= 'testrun_fullsoil_profile.nc')
+#outputfile, Model = driver(parameters=params, create_ncf=True, result_file= 'testrun_fullsoil_profile_moss_convergence_hackathon_day3.nc')
 
